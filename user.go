daae004623a704fe0eddfa5cc3e46645b063c01c
German lozada Silva <german_lozada@outsource.unicomer.com>
2024-03-30 13:52:36 -0500

    update

--- user.go
+++ user.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func user(arr []int) []int {
 
 	a := make([]int, 5)
@@ -8,4 +10,41 @@ func user(arr []int) []int {
 
 func dos() {
 
+}
+
+type Producto struct {
+	ID         int
+	Nombre     string
+	Precio     float64
+	Existencia int
+}
+
+// Función para buscar un producto por nombre
+func buscarProductoPorNombre(productos []Producto, nombre string) *Producto {
+	for _, producto := range productos {
+		if producto.Nombre == nombre {
+			return &producto
+		}
+	}
+	return nil // Si no se encuentra el producto, devolvemos nil
+}
+
+func main6() {
+	// Lista de productos de ejemplo
+	productos := []Producto{
+		{ID: 1, Nombre: "Producto1", Precio: 10.99, Existencia: 100},
+		{ID: 2, Nombre: "Producto2", Precio: 15.99, Existencia: 50},
+		{ID: 3, Nombre: "Producto3", Precio: 20.99, Existencia: 200},
+	}
+
+	// Buscar un producto por nombre
+	nombre := "Producto2"
+	productoEncontrado := buscarProductoPorNombre(productos, nombre)
+
+	// Verificar si se encontró el producto
+	if productoEncontrado != nil {
+		fmt.Printf("Producto encontrado: %+v\n", *productoEncontrado)
+	} else {
+		fmt.Println("Producto no encontrado.")
+	}
 }
